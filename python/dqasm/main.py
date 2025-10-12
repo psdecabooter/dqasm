309a29be8aef4fc10d186a95d230771c813b6402
Patrick DeCabooter <psdecabooter@gmail.com>
2025-10-11 19:53:20 -0500

    Setting up Pyo3

---
+++ python/dqasm/main.py
@@ -0,0 +1,4 @@
+import dqasm
+
+for _ in range(10):
+    print(dqasm.py_my_function())
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,18 @@
+use pyo3::prelude::*;
 pub mod qasm_parser;
 pub mod structures;
+
+pub fn my_function() -> String {
+    "Hello".to_string()
+}
+
+#[pyfunction]
+fn py_my_function() -> String {
+    my_function()
+}
+
+#[pymodule]
+fn dqasm(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(py_my_function, m)?)?;
+    Ok(())
+}
